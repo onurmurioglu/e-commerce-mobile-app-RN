4cf5565f3c63b7ec9a07774ce409d70149367fd6
onurmurioglu <onurmurioglu@gmail.com>
2023-05-13 20:24:22 +0300

    first

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+export default class App extends React.Component{
+  render() {
+    return(
+      <SafeAreaView>
+        <Text style={{color: 'red', fontWeight:'bold', fontSize: 20}}>MERHABAAA!!!</Text>
+      </SafeAreaView>
+    )
+  }
+}
